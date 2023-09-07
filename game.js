5013b6a226d2a56cc9aba148d7584243e663567e
Taymen Verschuren <Taymen@iinet.net.au>
2023-09-07 08:58:46 +0800

    3x3 all working

--- game.js
+++ game.js
@@ -5,21 +5,28 @@ const playerOneDisplay = document.querySelector('.playerOne');
 const playerTwoDisplay = document.querySelector('.playerTwo');
 const resetBtn = document.querySelector('.reset');
 const gameSquares = document.querySelectorAll('.board div');
+const xBtn = document.querySelector('#x')
+const oBtn = document.querySelector('#o')
+const gameDiv = document.querySelector('.gameWrapper')
+const selectionDiv =document.querySelector('.selection')
 
 let playerOneScore = playerOneDisplay.innerText;
 let playerTwoScore = playerTwoDisplay.innerText;
 
+let playerOneToken = ''
+let playerTwoToken
 let moveCounter = 0;
 let roundCounter = 1;
 let playerOneChoices = [];
 let playerTwoChoices = [];
 
+
 //checks if space is free then whose turn it is followed by incrememnting the move counter and lastly checks result after every turn
 function moveDecider(evt) {
-    if(evt.target.innerText === ''){
-        if(moveCounter %2 === 0){
+    if (evt.target.innerText === '') {
+        if (moveCounter % 2 === 0) {
             handleMoveP1(evt)
-        }else{
+        } else {
             handleMoveP2(evt)
         }
         moveCounter++;
@@ -29,15 +36,13 @@ function moveDecider(evt) {
 //displays players move selection and pushes it into the corrosponding array
 function handleMoveP1(evt) {
     let playerOneMove = evt.target;
-    playerOneMove.innerText = 'X';
+    playerOneMove.innerText = playerOneToken;
     playerOneChoices.push(Number(playerOneMove.dataset.index));
-    
 }
 function handleMoveP2(evt) {
     let playerTwoMove = evt.target
-    playerTwoMove.innerText = 'O'
+    playerTwoMove.innerText = playerTwoToken
     playerTwoChoices.push(Number(playerTwoMove.dataset.index))
-    
 }
 
 function checkResult() {
@@ -51,30 +56,30 @@ function checkResult() {
         [0, 4, 8],
         [2, 4, 6],
     ];
+
     for (let combination of winningCombinations) {
         let [a, b, c] = combination;
-        
+
         if (playerOneChoices.includes(a) && playerOneChoices.includes(b) && playerOneChoices.includes(c)) {
             titleMessage.innerText = 'Player 1 wins!';
             playerOneScore++;  
             disableClicks();
-            updateScores()     
-            
+            updateScores();
+            break
         }
         
-        if (playerTwoChoices.includes(a) && playerTwoChoices.includes(b) && playerTwoChoices.includes(c)) {
+        if (playerTwoChoices.includes(a) &&playerTwoChoices.includes(b) && playerTwoChoices.includes(c)) {
             titleMessage.innerText = 'Player 2 wins!';
             playerTwoScore++;
             disableClicks();
-            updateScores()
-            
+            updateScores();
+            break
         }
-        if (moveCounter === 9 && !(playerTwoChoices.includes(a) && playerTwoChoices.includes(b) && playerTwoChoices.includes(c)) && !(playerOneChoices.includes(a) && playerOneChoices.includes(b) && playerOneChoices.includes(c))) {
+
+        if (moveCounter === 9) {
             titleMessage.innerText = "It's a draw!";
-            
         }
     }
- 
 }
 
 function disableClicks() {
@@ -88,8 +93,8 @@ function updateScores() {
     playerTwoDisplay.innerText = playerTwoScore;
 }
 
-function handleReset(){
-    if(playerOneChoices.length > 2 || playerTwoChoices.length > 2){
+function handleReset() {
+    if (playerOneChoices.length > 2 || playerTwoChoices.length > 2){
         
         moveCounter = 0;
         playerOneChoices = [];
@@ -97,21 +102,35 @@ function handleReset(){
         roundCounter++
         titleMessage.innerText = `Round ${roundCounter}`
         
-        for(let square of gameSquares) {
+        for (let square of gameSquares) {
             square.addEventListener('click', moveDecider);
             square.innerText = ''
         }
-    }else{
+    } else {
         titleMessage.innerText = 'No sore Losers'
     }
 }
 
+function handleSelection(evt) {
+    if (evt.target === xBtn) {
+        playerOneToken = 'X';
+        playerTwoToken = 'O'
+        gameDiv.style.display = 'block'
+        selectionDiv.style.display = 'none'
+
+    } else if (evt.target === oBtn){
+        playerOneToken = 'O'
+        playerTwoToken = 'X'
+        gameDiv.style.display = 'block'
+        selectionDiv.style.display = 'none'
+    }
+    
+}
 //set up game board ready to respond to the user
 for(let square of gameSquares) {
     square.addEventListener('click', moveDecider);
 }
-resetBtn.addEventListener('click', handleReset)
-
-
-
+resetBtn.addEventListener('click', handleReset);
+xBtn.addEventListener('click', handleSelection)
+oBtn.addEventListener('click', handleSelection)
 
